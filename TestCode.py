452d2f20d9232d0db4f236d74364bebd14ea1006
Duankong_ubuntu <lxblearning@outlook.com>
2020-07-24 13:13:21 +0800

    support JPEG2000 test

--- TestCode.py
+++ TestCode.py
@@ -3,17 +3,19 @@ from CmdLine import JPEG, JPEG2000
 
 if __name__ == '__main__':
     bmpfile = 'testout.bmp'
-    jp2file = 'duankong_demo.jp2'
-    jpgfile = "duankong_demo.jpg"
-    decodefile = "decode.bmp"
+
+    jp2file = 'duankong_demo1.jp2'
+    jpgfile = "duankong_demo1.jpg"
+
+    decodefile = "decode_jp2.bmp"
     decodejpg = "decode_jpg.bmp"
     # JPEG
     jpeg = JPEG()
     jpeg.code_image(input=bmpfile, output=jpgfile)
     jpeg.decode_image(input=jpgfile, output=decodejpg)
     # JPEG2000
-    # code = JPEG2000()
-    # code.code_image(input=bmpfile,output=jp2file)
-    # code.decode_image(input=jp2file, output=decodefile, verbose=1)
+    code = JPEG2000()
+    code.code_image(input=bmpfile,output=jp2file)
+    code.decode_image(input=jp2file, output=decodefile)
 
     print("[ ] Doen !")
