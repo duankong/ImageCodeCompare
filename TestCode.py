b28f96a09fe73f2297bb5dfa7ce3155569bdab7a
Duankong_ubuntu <lxblearning@outlook.com>
2020-07-24 13:06:33 +0800

    modified TestCode

--- TestCode.py
+++ TestCode.py
@@ -1,8 +1,19 @@
 import os
-from CmdLine import JPEG
+from CmdLine import JPEG, JPEG2000
+
 if __name__ == '__main__':
-    print("hello world")
-    jpeg=JPEG()
-    inputfile="/home/duankong/图片/testout.bmp"
-    outputfile="/home/duankong/图片/duankong3.jpg"
-    jpeg.code_image(inputfile,outputfile)
+    bmpfile = 'testout.bmp'
+    jp2file = 'duankong_demo.jp2'
+    jpgfile = "duankong_demo.jpg"
+    decodefile = "decode.bmp"
+    decodejpg = "decode_jpg.bmp"
+    # JPEG
+    jpeg = JPEG()
+    jpeg.code_image(input=bmpfile, output=jpgfile)
+    jpeg.decode_image(input=jpgfile, output=decodejpg)
+    # JPEG2000
+    # code = JPEG2000()
+    # code.code_image(input=bmpfile,output=jp2file)
+    # code.decode_image(input=jp2file, output=decodefile, verbose=1)
+
+    print("[ ] Doen !")
