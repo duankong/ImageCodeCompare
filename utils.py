0adba1163e2a2b151846a3ccb70e8eb474ad24ff
Duankong_ubuntu <lxblearning@outlook.com>
2020-07-25 16:30:47 +0800

    update

--- utils.py
+++ utils.py
@@ -1,5 +1,12 @@
-def addroot(input, output, source_path="/home/duankong/test_image/", target_path="/home/duankong/test_image/"):
-    input = source_path + input
+import os
+
+
+def addroot(input, output, source_path="/home/duankong/test_image/sourceimage/",
+            target_path="/home/duankong/test_image/"):
+    if os.path.splitext(input)[-1] == ".bmp" or os.path.splitext(input)[-1] == ".png":
+        input = source_path + input
+    else:
+        input = target_path + input
     output = target_path + output
     return input, output
 
