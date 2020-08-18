ec9807e1a97d343da30e3090232661c6c7f143bc
duankong <lxblearning@gmail.com>
2020-08-18 21:57:36 +0800

    change

--- run.sh
+++ run.sh
@@ -1,8 +1,24 @@
 #！/bin/bash
+
+## init
 rm -rf /image_test/*
+rm -rf /code/image_test/
+rm -rf /code/encoding_result_*
+## PARAMETER
+metric="psnr_avg"
+db_file_name="encoding_results_${metric}.db"
+
+## COMPRESSION
 cd /image_test/
-python3 /code/script_compress_parallel.py
-#tree -C /image_test/
-#rm -rf /code/image_test/
-#cp -r /image_test/ /code/
+python3 /code/script_compress_parallel.py \
+--metric ${metric} \
+--db_file_name=${db_file_name} \
 #cp -f  /code/image_test/encoding_results* /code/
+
+## COMPUTER_BD_RATES 
+#python3 /code/computer_BD_rates.py --db_file_name=${db_file_name} \
+
+## ANALYZE ENCODING RESULT
+#python3 /code/analyze_encoding_results.py --metric ${metric} --db_file_name=${db_file_name} \
+
+#cp -r /image_test/ /code/
