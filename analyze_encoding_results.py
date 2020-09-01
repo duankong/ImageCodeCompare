1323f0e811e5cdf56f8c83936023f8888ad322d1
duankong <lxblearning@gmail.com>
2020-09-01 22:35:14 +0800

    2.0 lossless

--- analyze_encoding_results.py
+++ analyze_encoding_results.py
@@ -2,12 +2,49 @@ import sqlite3
 import statistics
 import sys
 import logging
+import numpy as np
 import ntpath
 import matplotlib.pyplot as plt
 from collections import defaultdict
 
-from utils import *
-from config import args_config
+from utils.utils_common import easy_logging, get_mean_metric_value_file_size_bytes, get_metric_value_file_size_bytes
+from utils.sqlcmd import get_unique_sorted, query_for_codec, apply_checks_before_analyzing, apply_size_check, \
+    get_unique_sorted_with_sub_sampling
+
+from config import args_analyze_config
+
+blanket = 10
+codec_len = 15
+
+
+def showresult(logger, codecs, sub_sampling, unique_sorted_metric_values, source_image_all, results_dict):
+    print('\n')
+
+    logger.info('=' * (blanket + 1 + codec_len * len(codecs)))
+    sub_sampling_report = '{} subsampling'.format(sub_sampling)
+    logger.info(sub_sampling_report)
+    logger.info('-' * len(sub_sampling_report))
+    codecs_string = ' ' * (blanket + 1)
+    for codec in codecs:
+        codecs_string += codec.rjust(codec_len)
+    logger.info(codecs_string)
+    for target in unique_sorted_metric_values:
+        all_codec_results, codec_results_terse, all_codec_results_terse_all = results_dict[target]
+        # === EVE === #
+        for i in range(len(all_codec_results_terse_all[0])):
+            consolidated_results = ""
+            for a in all_codec_results_terse_all:
+                consolidated_results += '{:.2f}%'.format(a[i]).rjust(codec_len)
+            logger.info('{}:{}'.format(str(source_image_all[i]).ljust(blanket), consolidated_results))
+        # === AVG === #
+        consolidated_results = ""
+        for a in codec_results_terse:
+            consolidated_results += a.ljust(codec_len)
+        logger.info('{}:{}'.format(str(target if not args_analyze_config().lossless else 'avg({})'.format(
+            len(all_codec_results_terse_all[0]))).ljust(blanket),
+                                   consolidated_results))
+    logger.info('=' * (blanket + 1 + codec_len * len(codecs)))
+    logger.info("\n\n")
 
 
 def get_mean_metric_print(metric_name, metric_value, vmaf_value):
@@ -30,92 +67,88 @@ def get_print_string(codec, sub_sampling, count, metric_value, file_size, metric
 
 
 def main():
-
-    metric_name=args_config().metric
-    db_file_name=args_config().db_file_name
-
-    connection = sqlite3.connect(db_file_name)
-
-    unique_sorted_metric_values, total_pixels = apply_checks_before_analyzing(connection, metric_name)
-
-    logger = easy_logging(file_prefix="bitrate_savings", db_file_name=db_file_name)
-
-    baseline_codec = 'jpeg'
-    sub_sampling_arr = get_unique_sorted(connection, "SUB_SAMPLING")
-    codecs = get_unique_sorted(connection, "CODEC")
-
-    plot_list = get_unique_sorted(connection, "CODEC")
+    # =================================== BASIC CONFIGS =================================== #
+    metric_name = args_analyze_config().metric
+    db_file_name = args_analyze_config().db_file_name
+    verbose = args_analyze_config().quiet
+    logger = easy_logging(file_prefix="bitrate_savings", db_file_name=db_file_name)  # logging
+    connection = sqlite3.connect(db_file_name)  # connect database
+    baseline_codec = 'jpeg'  # baseline
+    sub_sampling_arr = get_unique_sorted(connection, "SUB_SAMPLING")  # subsampling
     color_list = ['tab:blue', 'tab:orange', 'tab:green', 'tab:red', 'tab:purple', 'tab:brown', 'tab:pink', 'tab:gray',
                   'tab:olive', 'tab:cyan']
-    marker_list = ['o', 'v', '>', '<', 's', 'p', 'd', '4', 'P', 'X']
-
-    assert len(color_list) == len(marker_list)  # 10 curves on one plot is the limit, beyond that is sensory overload
-    assert len(plot_list) <= len(marker_list)
-
+    marker_list = ['o', 'v', '>', '<', 's', 'p', 'd', '4', 'P', 'X']  # 'D', '|', '_'
+    assert len(color_list) == len(marker_list)
+    # ===================================   CHECK DATA   =================================== #
+    if not args_analyze_config().lossless:
+        unique_sorted_metric_values, total_pixels = apply_checks_before_analyzing(connection, metric_name)
+    else:
+        total_pixels = apply_size_check(connection)
+        unique_sorted_metric_values = get_unique_sorted(connection, "TARGET_VALUE")
+    # =================================== ANALYZE  sub_sampling =================================== #
     for sub_sampling in sub_sampling_arr:
         results_dict = dict()
         results_bpp = defaultdict(list)
         results_quality = defaultdict(list)
+
+        codecs = get_unique_sorted_with_sub_sampling(connection, "CODEC", sub_sampling)
+        # ===================================     ANALYZE  target   =================================== #
         for target in unique_sorted_metric_values:
-            baseline_results = connection.execute(query_for_codec(baseline_codec, sub_sampling, metric_name, target)).fetchall()
+
+            baseline_results = connection.execute(
+                query_for_codec(baseline_codec, sub_sampling, metric_name, target)).fetchall()
 
             baseline_metric_value, baseline_file_size, baseline_count, baseline_vmaf_value = get_mean_metric_value_file_size_bytes(
                 baseline_results)
-            print('Baseline is ' + get_print_string(baseline_codec, sub_sampling, baseline_count, baseline_metric_value,
-                                                    baseline_file_size, metric_name, baseline_vmaf_value))
+
+            baseline_metric_values_all, baseline_file_size_all, baselne_vmaf_values_all, baseline_source_image_all = get_metric_value_file_size_bytes(
+                baseline_results)
+            if verbose == 0:
+                print('Baseline is ' + get_print_string(baseline_codec, sub_sampling, baseline_count,
+                                                        baseline_metric_value,
+                                                        baseline_file_size, metric_name, baseline_vmaf_value))
+
             results_bpp[baseline_codec].append(baseline_file_size * 8.0 / total_pixels)
             results_quality[baseline_codec].append(baseline_metric_value)
+
             results_list = list()
             results_list_terse = list()
-
+            results_list_terse_all = list()
+            # ===================================     ANALYZE  codec     =================================== #
             for codec in codecs:
-                if codec == 'webp' and sub_sampling == '444':
-                    continue
                 results = connection.execute(query_for_codec(codec, sub_sampling, metric_name, target)).fetchall()
                 metric_value, file_size, count, vmaf_value = get_mean_metric_value_file_size_bytes(results)
-                print(' Compared codec is ' + get_print_string(codec, sub_sampling, count, metric_value, file_size,
-                                                               metric_name, vmaf_value))
+                if verbose == 0:
+                    print(' Compared codec is ' + get_print_string(codec, sub_sampling, count, metric_value, file_size,
+                                                                   metric_name, vmaf_value))
+                    print('  Average reduction is {:.2f}%'.format(
+                        (file_size - baseline_file_size) / baseline_file_size * 100.0))
                 # negative is better. Positive means increase in file_size
-                print('  Average reduction is {:.2f}%'.format(
-                    (file_size - baseline_file_size) / baseline_file_size * 100.0))
                 results_list.append(
                     '{} {:.2f}%'.format(codec, (file_size - baseline_file_size) / baseline_file_size * 100.0))
                 results_list_terse.append(
-                    '{:.2f}%'.format((file_size - baseline_file_size) / baseline_file_size * 100.0).rjust(16))
+                    '{:.2f}%'.format((file_size - baseline_file_size) / baseline_file_size * 100.0).rjust(codec_len))
+                metric_values_all, file_size_all, vmaf_values_all, source_image_all = get_metric_value_file_size_bytes(
+                    results)
+                assert source_image_all == baseline_source_image_all
+                results_list_terse_all.append((np.array(file_size_all) - np.array(baseline_file_size_all)) / np.array(
+                    baseline_file_size_all) * 100)
+
                 results_bpp[codec].append(file_size * 8.0 / total_pixels)
                 results_quality[codec].append(metric_value)
-            results_dict[target] = (results_list, results_list_terse)
+
+            results_dict[target] = (results_list, results_list_terse, results_list_terse_all)
             print("")
+        # ===================================  SHOW  RESULT  =================================== #
+        showresult(logger, codecs, sub_sampling, unique_sorted_metric_values, source_image_all, results_dict)
 
-        print('\n')
-        logger.info('=' * (8 + 16 * len(codecs)))
-        sub_sampling_report = '{} subsampling'.format(sub_sampling)
-        logger.info(sub_sampling_report)
-        logger.info('-' * len(sub_sampling_report))
-        codecs_string = ' ' * 8
-        for codec in codecs:
-            if codec == 'webp' and sub_sampling == '444':
-                continue
-            codecs_string += codec.rjust(16)
-        logger.info(codecs_string)
-        for target in unique_sorted_metric_values:
-            all_codec_results, all_codec_results_terse = results_dict[target]
-            consolidated_results = ""
-            for a in all_codec_results_terse:
-                consolidated_results += a.ljust(16)
-            logger.info('{} : {}'.format(str(target).ljust(5), consolidated_results))
-        logger.info('=' * (8 + 16 * len(codecs)))
-        logger.info("\n\n")
-
+        # ===================================     PLOT     =================================== #
         fig = plt.figure(figsize=(12.8, 7.2))
         marker_num = 0
         plt.plot(results_bpp[baseline_codec], results_quality[baseline_codec], linewidth=2.0,
                  color=color_list[marker_num], marker=marker_list[marker_num], label=baseline_codec)
         for codec in codecs:
-            if codec in plot_list:
-                if codec == 'webp' and sub_sampling == '444':
-                    marker_num += 1
-                    continue
+            if codec not in baseline_codec and results_quality[codec][0] < float("inf"):
                 marker_num += 1
                 plt.plot(results_bpp[codec], results_quality[codec], linewidth=2.0, color=color_list[marker_num],
                          marker=marker_list[marker_num], label=codec)
